b11f157e1f3ef5361247b24713589d985f6fc291
BaldBodyBuilder <conorxdevlin@gmail.com>
2021-08-15 18:03:37 -0500

    Completed Homework Week08
    Did all of the 'close' workings

---
+++ Cohort_2/Week8/TokenSaleVer2.hs
@@ -0,0 +1,187 @@
+-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DeriveAnyClass        #-}
+{-# LANGUAGE DeriveGeneric         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeOperators         #-}
+
+module Week08.TokenSale
+    ( TokenSale (..)
+    , TSRedeemer (..)
+    , TSStartSchema
+    , TSUseSchema
+    , startEndpoint
+    , useEndpoints
+    ) where
+
+import           Control.Monad                hiding (fmap)
+import           Data.Aeson                   (FromJSON, ToJSON)
+import           Data.Monoid                  (Last (..))
+import           Data.Text                    (Text, pack)
+import           GHC.Generics                 (Generic)
+import           Plutus.Contract              as Contract
+import           Plutus.Contract.StateMachine
+import qualified PlutusTx
+import           PlutusTx.Prelude             hiding (Semigroup(..), check, unless)
+import           Ledger                       hiding (singleton)
+import           Ledger.Ada                   as Ada
+import           Ledger.Constraints           as Constraints
+import qualified Ledger.Typed.Scripts         as Scripts
+import           Ledger.Value
+import           Prelude                      (Semigroup (..), Show (..), uncurry)
+import qualified Prelude
+-- Locking tokens into a contract and setting a price allowing other people to purchase said tokens
+-- Starts with a NFT
+{-
+Seller locks the NFT at the UTXO address, Setting price to a different value by submitting a transaction with the current utxo as input and new utxo as output with the datum as the output change
+Adding tokens requires a lock into the contract, he must create a TX that has as input the original utxo and the tokens to be sold and the new output which contains the update tokens and the price.
+Buyer offers 12 ADA and for Token T at a price of 1T/6ADA and the output would be 3T (-2T) , 12ADA & 2T(+2T) , 0 ADA (-12ADA)
+To get a transaction to withdraw all the ADA that has been deposited within the contract he'd simply need to create a new UTXO to extract the ADA
+
+-}
+
+
+data TokenSale = TokenSale
+    { tsSeller :: !PubKeyHash
+    , tsToken  :: !AssetClass -- Being sold
+    , tsTT     :: !(Maybe ThreadToken) -- What you would use in production
+    } deriving (Show, Generic, FromJSON, ToJSON, Prelude.Eq)
+
+PlutusTx.makeLift ''TokenSale
+
+data TSRedeemer =
+      SetPrice Integer
+    | AddTokens Integer
+    | BuyTokens Integer
+    | Withdraw Integer Integer -- tokens to withdarw and lovelace to withdraw
+    | Close
+    deriving (Show, Prelude.Eq)
+
+PlutusTx.unstableMakeIsData ''TSRedeemer
+
+{-# INLINABLE lovelaces #-}
+lovelaces :: Value -> Integer
+lovelaces = Ada.getLovelace . Ada.fromValue
+
+{-# INLINABLE transition #-}
+transition :: TokenSale -> State Integer -> TSRedeemer -> Maybe (TxConstraints Void Void, State Integer)
+transition ts s r = case (stateValue s, stateData s, r) of
+    (v, _, SetPrice p)   | p >= 0           -> Just ( Constraints.mustBeSignedBy (tsSeller ts) -- Set price transition
+                                                    , State p v
+                                                    )
+    (v, p, AddTokens n)  | n > 0            -> Just ( mempty                                   -- Add tokens, demanding the amount of tokens to add is positive, written from the point of view of the sellar, adding n tokens
+                                                    , State p $
+                                                      v                                       <>
+                                                      assetClassValue (tsToken ts) n           
+                                                    )
+    (v, p, BuyTokens n)  | n > 0            -> Just ( mempty                                    -- Buy tokens, no constraints
+                                                    , State p $
+                                                      v                                       <>
+                                                      assetClassValue (tsToken ts) (negate n) <>
+                                                      lovelaceValueOf (n * p)
+                                                    )
+    (v, p, Withdraw n l) | n >= 0 && l >= 0 -> Just ( Constraints.mustBeSignedBy (tsSeller ts)      -- Withdraw, one must be greater than zero
+                                                    , State p $
+                                                      v                                       <>
+                                                      assetClassValue (tsToken ts) (negate n) <> -- Negate is the subtracting of the values from the transaction
+                                                      lovelaceValueOf (negate l)
+                                                    )
+    (_, Just _, Close)                      -> Just ( Constraints.mustBeSignedBy (tsSeller ts) -- > Solution, disregarding the TS and the actual state the moment the redeemer is called as a close then it can start closing
+                                                    , State Nothing $
+                                                      mempty
+                                                    +)
+    _                                       -> Nothing
+
+{-# INLINABLE tsStateMachine #-}
+tsStateMachine :: TokenSale -> StateMachine Integer TSRedeemer  
+tsStateMachine ts = mkStateMachine (tsTT ts) (transition ts) (const False) -- Thread token transition function and if the state is final or not, once it starts it will run forever
+
+{-# INLINABLE mkTSValidator #-}
+mkTSValidator :: TokenSale -> Integer -> TSRedeemer -> ScriptContext -> Bool
+mkTSValidator = mkValidator . tsStateMachine
+
+type TS = StateMachine Integer TSRedeemer
+
+tsTypedValidator :: TokenSale -> Scripts.TypedValidator TS
+tsTypedValidator ts = Scripts.mkTypedValidator @TS
+    ($$(PlutusTx.compile [|| mkTSValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode ts)
+    $$(PlutusTx.compile [|| wrap ||])
+  where
+    wrap = Scripts.wrapValidator @Integer @TSRedeemer
+
+tsValidator :: TokenSale -> Validator
+tsValidator = Scripts.validatorScript . tsTypedValidator
+
+tsAddress :: TokenSale -> Ledger.Address
+tsAddress = scriptAddress . tsValidator
+
+tsClient :: TokenSale -> StateMachineClient Integer TSRedeemer
+tsClient ts = mkStateMachineClient $ StateMachineInstance (tsStateMachine ts) (tsTypedValidator ts)
+
+mapErrorSM :: Contract w s SMContractError a -> Contract w s Text a
+mapErrorSM = mapError $ pack . show -- using text as the error messages
+
+--Offchain code
+-- token for sale, bool is the thread token if true to pinpoint the correct thread token
+startTS :: AssetClass -> Bool -> Contract (Last TokenSale) s Text ()
+startTS token useTT = do
+    pkh <- pubKeyHash <$> Contract.ownPubKey
+    tt  <- if useTT then Just <$> mapErrorSM getThreadToken else return Nothing -- if not use nothing, we could use get thread token function but need to use Maperrorsm to convert ot test
+    let ts = TokenSale
+            { tsSeller = pkh --myself thus own pubkey hash
+            , tsToken  = token
+            , tsTT     = tt
+            }
+        client = tsClient ts
+    void $ mapErrorSM $ runInitialise client 0 mempty -- client datum value, initial price being 0 and mempty holding no value thus 0
+    tell $ Last $ Just ts --  value of ts depends of the value of thread token
+    logInfo $ "started token sale " ++ show ts
+--Button liners, token sale and the integer with runstep which causes 1 transition in the state machine
+setPrice :: TokenSale -> Integer -> Contract w s Text ()
+setPrice ts p = void $ mapErrorSM $ runStep (tsClient ts) $ SetPrice p
+
+addTokens :: TokenSale -> Integer -> Contract w s Text ()
+addTokens ts n = void $ mapErrorSM $ runStep (tsClient ts) $ AddTokens n
+
+buyTokens :: TokenSale -> Integer -> Contract w s Text ()
+buyTokens ts n = void $ mapErrorSM $ runStep (tsClient ts) $ BuyTokens n
+
+withdraw :: TokenSale -> Integer -> Integer -> Contract w s Text ()
+withdraw ts n l = void $ mapErrorSM $ runStep (tsClient ts) $ Withdraw n l
+-- Close addition
+close :: TokenSale -> Contract w s Text ()
+close ts = void $ mapErrorSm $ runStep (tsClient ts) Close
+--using two schemes one to start one to interact
+type TSStartSchema =
+        Endpoint "start"      (CurrencySymbol, TokenName, Bool)
+type TSUseSchema =
+        Endpoint "set price"  Integer
+    .\/ Endpoint "add tokens" Integer
+    .\/ Endpoint "buy tokens" Integer
+    .\/ Endpoint "withdraw"   (Integer, Integer) -- pair of integers for the Token , ADA to withdraw
+    .\/ Endpoint "close"      ()
+-- new endpoint endpoint :: forall a w s e b || a -> Contract w s e b. Waiting for the a with return type b
+-- Promise w s e b \\ writer redeemer error b -> promise is a contract that waits for external input
+startEndpoint :: Contract (Last TokenSale) TSStartSchema Text ()
+startEndpoint = forever
+              $ handleError logError
+              $ awaitPromise
+              $ endpoint @"start" $ \(cs, tn, useTT) -> startTS (AssetClass (cs, tn)) useTT -- triple of currency symbol transaction and Use tt to being the TS, gives you a promise that will lock it away
+
+useEndpoints :: TokenSale -> Contract () TSUseSchema Text ()
+useEndpoints ts = forever
+                $ handleError logError
+                $ awaitPromise
+                $ setPrice' `select` addTokens' `select` buyTokens' `select` withdraw' `select` close' -- promise that waits until there is input then begins the contract
+  where -- endpoints and thus promises
+    setPrice'  = endpoint @"set price"  $ setPrice ts
+    addTokens' = endpoint @"add tokens" $ addTokens ts
+    buyTokens' = endpoint @"buy tokens" $ buyTokens ts
+    withdraw'  = endpoint @"withdraw"   $ uncurry (withdraw ts)
+    close'     = endpoint @"close"      $ close ts
